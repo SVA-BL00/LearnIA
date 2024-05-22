f6816efafdf076aef4dc18ceb0a72e5fa0f1921e
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-05-21 23:41:16 -0600

    WIP of background animation

--- LearnIA/app/routes/_main.MisCursos.jsx
+++ LearnIA/app/routes/_main.MisCursos.jsx
@@ -1,50 +1,50 @@
-import { useState, useEffect } from 'react';
-import { useLocation } from '@remix-run/react';
-import '../styles/MisCursos.css';
+import { useState, useEffect } from "react";
+import { useLocation } from "@remix-run/react";
+import "../styles/MisCursos.css";
 
 function MisCursos() {
-    const location = useLocation();
-    const [activePath, setActivePath] = useState(location.pathname);
-    const [isCollapsibleActive, setIsCollapsibleActive] = useState(false);
+	const location = useLocation();
+	const [activePath, setActivePath] = useState(location.pathname);
+	const [isCollapsibleActive, setIsCollapsibleActive] = useState(false);
 
-    useEffect(() => {
-        setActivePath(location.pathname);
-    }, [location.pathname]);
+	useEffect(() => {
+		setActivePath(location.pathname);
+	}, [location.pathname]);
 
-    const handleCollapsibleClick = () => {
-        setIsCollapsibleActive(!isCollapsibleActive);
-    };
+	const handleCollapsibleClick = () => {
+		setIsCollapsibleActive(!isCollapsibleActive);
+	};
 
-    return (
-        <div>
-            <div className="MisCursos">
-                <div className="container-fluid mt-5 mb-4">
-                    <h1>Mis Cursos</h1>
-                </div>
-            </div>
+	return (
+		<div>
+			<div className="MisCursos">
+				<div className="container-fluid mt-5 mb-4">
+					<h1>Mis Cursos</h1>
+				</div>
+			</div>
 
-            <div>
-              <hr className="solid" />
-            </div>
+			<div>
+				<hr className="solid" />
+			</div>
 
-            <button 
-                type="button" 
-                className={`collapsible ${isCollapsibleActive ? 'active' : ''}`} 
-                onClick={handleCollapsibleClick}
-            >
-                PROGRAMACIÓN ORIENTADA A OBJETOS
-                <div className="symbol-wrapper">
-                    <span className="symbol">&#x2329;</span>
-                </div>
-            </button>
-            <div 
-                className={`content ${isCollapsibleActive ? 'active' : ''}`} 
-                style={{ display: isCollapsibleActive ? 'block' : 'none' }}
-            >
-                <p>Lorem ipsum...</p>
-            </div>
-        </div>
-    );
+			<button
+				type="button"
+				className={`collapsible ${isCollapsibleActive ? "active" : ""}`}
+				onClick={handleCollapsibleClick}
+			>
+				PROGRAMACIÓN ORIENTADA A OBJETOS
+				<div className="symbol-wrapper">
+					<span className="symbol">&#x2329;</span>
+				</div>
+			</button>
+			<div
+				className={`content ${isCollapsibleActive ? "active" : ""}`}
+				style={{ display: isCollapsibleActive ? "block" : "none" }}
+			>
+				<p>Lorem ipsum...</p>
+			</div>
+		</div>
+	);
 }
 
 export default MisCursos;
