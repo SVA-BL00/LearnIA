7c1318422fbd3b04bdf571de29a041106345d62e
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-05-23 23:19:57 -0600

    Doing countdown by hand

--- LearnIA/app/components/Countdown.jsx
+++ LearnIA/app/components/Countdown.jsx
@@ -1,42 +1,28 @@
-import { useEffect } from "react";
-import "../styles/flip.min.css";
-import "../services/flip.min";
+import "../styles/countdown.css";
 
 function Countdown() {
-  useEffect(() => {
-    const handleTickInit = () => {
-        function handleTickInit(tick) {
-            var nextYear = (new Date()).getFullYear() + 1;
-            
-            Tick.count.down(nextYear + '-01-01').onupdate = function(value) {
-              tick.value = value;
-            };
-          }
-    };
 
-    handleTickInit();
-  }, []);
-
-  return (
-    <div className="tick" data-did-init="handleTickInit">
-      <div
-        data-repeat="true"
-        data-layout="horizontal center fit"
-        data-transform="preset(d, h, m, s) -> delay"
-      >
-        <div className="tick-group">
-          <div
-            data-key="value"
-            data-repeat="true"
-            data-transform="pad(00) -> split -> delay"
-          >
-            <span data-view="flip"></span>
-          </div>
-          <span data-key="label" data-view="text" className="tick-label"></span>
+    return (
+        <div className="mainClock">
+            <div className="box">
+                <div className="value"><span>30</span></div>
+                <span className="label">Days</span>
+            </div>
+            <div className="box">
+                <div className="value"><span>30</span></div>
+                <span className="label">Hours</span>
+            </div>
+            <div className="box">
+                <div className="value"><span>30</span></div>
+                <span className="label">Minutes</span>
+            </div>
+            <div className="box">
+                <div className="value"><span>30</span></div>
+                <span className="label">Seconds</span>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Countdown;
+
