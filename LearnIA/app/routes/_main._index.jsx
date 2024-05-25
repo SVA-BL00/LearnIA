ce2f08a216722f57c95ee920cfe41912ba40f051
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-05-24 23:00:49 -0600

    Top bar dashboard

--- LearnIA/app/routes/_main._index.jsx
+++ LearnIA/app/routes/_main._index.jsx
@@ -1,10 +1,23 @@
 import Countdown from "../components/Countdown";
+import Notification from "../components/Notification";
+import "../styles/main.css"
 function index() {
 	return (
+		//<div style={{marginLeft:'400px'}}>
 		<div className="dashboard">
 			<div className="container-fluid">
+			
+				<div className="wrapper">
+					<div className="wrapper-main">
+					<div className="title-wrapper">
+					<h2 className="title">¿Listo para tu examen?</h2>
+					</div>
+					
 				<Countdown />
-				<h1>Dashboard</h1>
+					</div>
+					
+				<Notification/>
+				</div>
 			</div>
 		</div>
 	);
