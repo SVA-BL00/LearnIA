b2a5c00df3b631a3010736d9d94c435676a41758
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-05-24 23:00:49 -0600

    Top bar dashboard

---
+++ LearnIA/app/components/Notification.jsx
@@ -0,0 +1,17 @@
+import "../styles/notification.css";
+
+function Notification() {
+  return (
+    <div className="wrapper-notif">
+      <div className="bell-notif">
+        <i className="bi bi-bell bell-icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
+
+
+  
+  
