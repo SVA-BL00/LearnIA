e0fd45414ce6b3cf6423dfdb5b843d178b33576a
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-05-27 14:10:26 -0600

    Fixed dashboard format

--- LearnIA/app/routes/_main.MisCursos.jsx
+++ LearnIA/app/routes/_main.MisCursos.jsx
@@ -1,75 +1,82 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "@remix-run/react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "../styles/MisCursos.css";   
+import "../styles/MisCursos.css";
 import CollapsibleSection from "../components/CollapsibleSection";
 import TitleWithImages from "../components/TitleWithImages";
 
 function MisCursos() {
-    const location = useLocation();
-    const [activePath, setActivePath] = useState(location.pathname);
+	const location = useLocation();
+	const [activePath, setActivePath] = useState(location.pathname);
 
-    useEffect(() => {
-        setActivePath(location.pathname);
-    }, [location.pathname]);
+	useEffect(() => {
+		setActivePath(location.pathname);
+	}, [location.pathname]);
 
 	return (
-		<div style={{marginLeft: '400px'}}>
+		<div style={{ marginLeft: "400px" }}>
 			<TitleWithImages title="Mis Cursos" />
-            <div>
-                <CollapsibleSection title="PROGRAMACIÓN ORIENTADA A OBJETOS II">
-                    <div className="collapsible-content">
-                        <div className="left-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum faucibus mollis. Mauris vel lacinia libero. Proin luctus semper sem, ac posuere velit.</p>
-                            <button className="btn orange">Ver temas</button>
-                            <button className="btn green">Proyectos recomendados</button>
-                            <button className="btn red">Abandonar curso</button>
-                        </div>
-                        <div className="right-content">
-                            <div style={{ width: 100, height: 100 }}>
-                                <CircularProgressbar 
-                                    value={63} 
-                                    text={`${63}%`}
-                                    styles={buildStyles({
-                                        textColor: "#2b8a74",
-                                        pathColor: "#2b8a74",
-                                        trailColor: "#d6d6d6"
-                                    })}
-                                />
-                            </div>
-                            <div className="progress-text">63% de progreso</div>
-                        </div>
-                    </div>
-                </CollapsibleSection>
+			<div>
+				<CollapsibleSection title="PROGRAMACIÓN ORIENTADA A OBJETOS II">
+					<div className="collapsible-content">
+						<div className="left-content">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+								elementum faucibus mollis. Mauris vel lacinia libero. Proin
+								luctus semper sem, ac posuere velit.
+							</p>
+							<button className="btn orange">Ver temas</button>
+							<button className="btn green">Proyectos recomendados</button>
+							<button className="btn red">Abandonar curso</button>
+						</div>
+						<div className="right-content">
+							<div style={{ width: 100, height: 100 }}>
+								<CircularProgressbar
+									value={63}
+									text={`${63}%`}
+									styles={buildStyles({
+										textColor: "#2b8a74",
+										pathColor: "#2b8a74",
+										trailColor: "#d6d6d6",
+									})}
+								/>
+							</div>
+							<div className="progress-text">63% de progreso</div>
+						</div>
+					</div>
+				</CollapsibleSection>
 
-
-                <CollapsibleSection title="BIOLOGÍA COMPUTACIONAL">
-                    <div className="collapsible-content">
-                        <div className="left-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum faucibus mollis. Mauris vel lacinia libero. Proin luctus semper sem, ac posuere velit.</p>
-                            <button className="btn orange">Ver temas</button>
-                            <button className="btn green">Proyectos recomendados</button>
-                            <button className="btn red">Abandonar curso</button>
-                        </div>
-                        <div className="right-content">
-                            <div style={{ width: 100, height: 100 }}>
-                                <CircularProgressbar 
-                                    value={63} 
-                                    text={`${63}%`}
-                                    styles={buildStyles({
-                                        textColor: "#2b8a74",
-                                        pathColor: "#2b8a74",
-                                        trailColor: "#d6d6d6"
-                                    })}
-                                />
-                            </div>
-                            <div className="progress-text">63% de progreso</div>
-                        </div>
-                    </div>
-                </CollapsibleSection>
-            </div>
-        </div>
-    );
+				<CollapsibleSection title="BIOLOGÍA COMPUTACIONAL">
+					<div className="collapsible-content">
+						<div className="left-content">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+								elementum faucibus mollis. Mauris vel lacinia libero. Proin
+								luctus semper sem, ac posuere velit.
+							</p>
+							<button className="btn orange">Ver temas</button>
+							<button className="btn green">Proyectos recomendados</button>
+							<button className="btn red">Abandonar curso</button>
+						</div>
+						<div className="right-content">
+							<div style={{ width: 100, height: 100 }}>
+								<CircularProgressbar
+									value={63}
+									text={`${63}%`}
+									styles={buildStyles({
+										textColor: "#2b8a74",
+										pathColor: "#2b8a74",
+										trailColor: "#d6d6d6",
+									})}
+								/>
+							</div>
+							<div className="progress-text">63% de progreso</div>
+						</div>
+					</div>
+				</CollapsibleSection>
+			</div>
+		</div>
+	);
 }
 
 export default MisCursos;
