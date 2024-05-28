3fe2dab8f245d0d82f8bd0c07142acaba5b02170
AdelaSolorio <a01637205@tec.mx>
2024-05-28 15:35:32 -0600

    Changed the database connection to optimize

--- LearnIA/app/services/db.js
+++ LearnIA/app/services/db.js
@@ -1,29 +1,26 @@
 import mysql from 'mysql2/promise';
+import { config } from "dotenv";
 
-let connection;
+config();
 
 export async function getConnection() {
-  if (!connection) {
-    connection = await mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE
-    });
-  }
-  return connection;
-}
-
-export async function closeConnection() {
-  if (connection) {
-    await connection.end();
-    connection = null;
-  }
+  const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+    maxIdle: 10, // max idle pools, the default value is the same as `poolLimit`
+    idleTimeout: 60000, // idle pools timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+  });
+  return pool;
 }
 
 export async function getCursosInscritos(estudianteId) {
-    const connection = await getConnection();
-    const [rows] = await connection.query('SELECT idCurso FROM Curso WHERE idEstudiante = ?', [estudianteId]);
+    const pool = await getConnection();
+    const [rows] = await pool.query('SELECT idCurso FROM Curso WHERE idEstudiante = ?', [estudianteId]);
     if (!rows || rows.length === 0) {
         return [];
     }
@@ -34,25 +31,25 @@ export async function getCursosByIds(cursoIds) {
     if (cursoIds.length === 0) {
         return [];
     }
-    const connection = await getConnection();
-    const [rows] = await connection.query('SELECT * FROM Curso WHERE idCurso IN (?)', [cursoIds]);
+    const pool = await getConnection();
+    const [rows] = await pool.query('SELECT * FROM Curso WHERE idCurso IN (?)', [cursoIds]);
     return rows;
 }
 
 export async function getCursoById(cursoId) {
-    const connection = await getConnection();
-    const [rows] = await connection.query('SELECT * FROM Curso WHERE idCurso = ?', [cursoId]);
+    const pool = await getConnection();
+    const [rows] = await pool.query('SELECT * FROM Curso WHERE idCurso = ?', [cursoId]);
     return rows[0];
 }
 
 export async function getNombreMateriaByCursoId(cursoId) {
-    const connection = await getConnection();
-    const [rows] = await connection.query('SELECT nombre FROM Materia WHERE idMateria = (SELECT idMateria FROM Curso WHERE idCurso = ?)', [cursoId]);
+    const pool = await getConnection();
+    const [rows] = await pool.query('SELECT nombre FROM Materia WHERE idMateria = (SELECT idMateria FROM Curso WHERE idCurso = ?)', [cursoId]);
     return rows[0].nombre;
 }
 
 export async function getDescripcionMateriaByCursoId(cursoId) {
-    const connection = await getConnection();
-    const [rows] = await connection.query('SELECT descripcion FROM Materia WHERE idMateria = (SELECT idMateria FROM Curso WHERE idCurso = ?)', [cursoId]);
+    const pool = await getConnection();
+    const [rows] = await pool.query('SELECT descripcion FROM Materia WHERE idMateria = (SELECT idMateria FROM Curso WHERE idCurso = ?)', [cursoId]);
     return rows[0].descripcion;
 }
