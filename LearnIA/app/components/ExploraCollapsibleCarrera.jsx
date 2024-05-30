e51fc22624f9e73eab7622ae0eb4b6064b72d9e6
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-05-29 21:37:56 -0600

    FrontExplora

---
+++ LearnIA/app/components/ExploraCollapsibleCarrera.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import "../styles/ExploraCollapsibleSection.css";
+
+function ExploraCollapsibleCarrera({ title, children }) {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleToggle = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <div className="collapsible1-section">
+            <button
+                type="button"
+                className={`collapsible1 ${isActive ? "active" : ""}`}
+                onClick={handleToggle}
+            >
+                {title}
+                <div className="symbol-wrapper">
+                    <span className="symbol1">&#x2329;</span>
+                </div>
+            </button>
+            <div className={`content1 ${isActive ? "active" : ""}`}>{children}</div>
+        </div>
+    );
+}
+
+export default ExploraCollapsibleCarrera;
+
+
