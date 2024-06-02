4a44c1109968ae1c553f91353e9906671aff12d2
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-06-01 22:29:16 -0600

    FunctionSimulated

--- LearnIA/app/routes/_main.explora.jsx
+++ LearnIA/app/routes/_main.explora.jsx
@@ -8,32 +8,43 @@ import ExploraCollapsibleSemestre from "../components/ExploraCollapsibleSemestre
 import ExploraCollapsibleMateria from "../components/ExploraCollapsibleMateria";
 
 function explora() {
-
-	return (
-		<div style={{ marginLeft: "400px" }}>
-			<TitleWithImages title="Explora" />
-				<ExploraCollapsibleCarrera title="ITC">
-					<div className="collapsible1-content">
-
-						<ExploraCollapsibleSemestre title="Semestre">
-							<div className="collapsible2-content">
-
-								<ExploraCollapsibleMateria title="Materia">
-									<div className="collapsible3-content">
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-											elementum faucibus mollis. Mauris vel lacinia libero. Proin
-											luctus semper sem, ac posuere velit.
-										</p>
-										<button className="btn green">Inscribirse</button>
-									</div>
-								</ExploraCollapsibleMateria>
-							</div>
-						</ExploraCollapsibleSemestre>
-					</div>
-				</ExploraCollapsibleCarrera>
-		</div>
-	);
+const courses = [
+	{
+		"semesters": [
+		  {
+			"name": "Semestre 2",
+			"subjects": ["Programación Orientada a Objetos", "Modelación de sistemas electromagnéticos", "Biología computacional"]
+		  },
+		  {
+			"name": "Semestre 3",
+			"subjects": ["Algoritmos fundamentales", "Internet de las cosas", "Ecuaciones diferenciales"]
+		  }
+		]
+	  }
+	];
+  return (
+    <div style={{ marginLeft: "400px" }}>
+      <TitleWithImages title="Explora" />
+      <ExploraCollapsibleCarrera title="ITC">
+        <div className="collapsible1-content">
+          {courses[0].semesters.map((semester, index) => (
+            <ExploraCollapsibleSemestre key={index} title={semester.name}>
+              <div className="collapsible2-content">
+                {semester.subjects.map((subject, subIndex) => (
+                  <ExploraCollapsibleMateria key={subIndex} title={subject}>
+                    <div className="collapsible3-content">
+                      <p>{subject}</p>
+                      <button className="btn green">Inscribirse</button>
+                    </div>
+                  </ExploraCollapsibleMateria>
+                ))}
+              </div>
+            </ExploraCollapsibleSemestre>
+          ))}
+        </div>
+      </ExploraCollapsibleCarrera>
+    </div>
+  );
 }
 
 export default explora;
