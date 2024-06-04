49d744be5434d2fbdd3d8858604eb45e2cf91647
Pamela Sanchez <pamela.sann@hotmail.com>
2024-06-04 12:40:23 -0600

    Return to original Perfil state, user information on Perfil and SideNav

--- LearnIA/app/services/auth.server.js
+++ LearnIA/app/services/auth.server.js
@@ -3,7 +3,6 @@ import { Authenticator } from "remix-auth";
 import { GoogleStrategy, SocialsProvider } from "remix-auth-socials";
 import { sessionStorage } from "../services/session.server";
 import { config } from "dotenv";
-import prisma from "./prisma/prisma.js";
 
 config();
 
@@ -24,36 +23,16 @@ authenticator.use(
 			callbackURL: getCallback(SocialsProvider.GOOGLE),
 		},
 		async ({ profile }) => {
-			// console.log(profile);
-			try {
-				let estudiante = await prisma.estudiante.findUnique({
-					where: {
-					  	correo: profile.emails[0].value,
-					},
-				});
+			//console.log(profile);
+				
+			const user = {
+			displayName: profile.displayName,
+			email: profile.emails[0].value,
+			photo: profile.photos[0].value,
+			};
 
-				if (!estudiante) {
-					// User does not exist, create a new entry
-					estudiante = await prisma.estudiante.create({
-						data: {
-						nombre: profile.displayName,
-						correo: profile.emails[0].value,
-						},
-					});
-				}
-        
-        const user = {
-          displayName: profile.displayName,
-          email: profile.emails[0].value,
-          photo: profile.photos[0].value,
-        };
-
-          // Return the profile object with estudianteId
-          return { ...profile, estudianteId: estudiante.idEstudiante, user };
-			} catch (error) {
-				console.error("Error during authentication:", error);
-        		throw new Error("Failed to authenticate user");
-			} 
+			// Return the profile object with estudianteId
+			return user;
 		},
 	),
 );
