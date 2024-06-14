47794ed9890f16adeac62797e47654f96bd7321e
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-06-13 18:57:36 -0600

    Added styles to notif

--- LearnIA/app/components/Notification.jsx
+++ LearnIA/app/components/Notification.jsx
@@ -1,13 +1,48 @@
+import { useState } from 'react';
 import "../styles/notification.css";
 
+const notifications = [
+    {
+        date: '2024-06-01',
+        message: 'Próximo quiz el 10 de Junio de 2024',
+        type: 'deadline',
+    },
+    {
+        date: '2024-06-05',
+        message: 'Se ha completado el quiz 3. Calificación: 80.5%',
+        type: 'completion',
+    },
+    {
+        date: '2024-06-10',
+        message: 'Próximo quiz el 20 de Junio de 2024',
+        type: 'deadline',
+    },
+];
+
 function Notification() {
-	return (
-		<div className="wrapper-notif">
-			<div className="bell-notif">
-				<i className="bi bi-bell bell-icon" />
-			</div>
-		</div>
-	);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleNotificationWindow = () => {
+        setIsVisible(!isVisible);
+    };
+
+    return (
+        <div className="wrapper-notif">
+            <button className="bell-notif" onClick={toggleNotificationWindow}>
+                <i className="bi bi-bell bell-icon" />
+            </button>
+            {isVisible && (
+                <div className="notification-window">
+                    {notifications.map((notification, index) => (
+                        <div key={index} className="notification">
+                            <p className={`date-notif ${notification.type}`}>{notification.date}</p>
+                            <p className="notif-name">{notification.message}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Notification;
