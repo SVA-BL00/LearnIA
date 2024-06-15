c69e2ebf1bbbe9fbcc2cab9eb9ed7e231d421aec
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-06-14 19:41:50 -0600

    Fixed miscursos dashboard

--- LearnIA/app/components/InfoMisCursos.jsx
+++ LearnIA/app/components/InfoMisCursos.jsx
@@ -1,5 +1,3 @@
-// app/components/InfoMisCursos.jsx
-
 import { useEffect, useState } from "react";
 import { useLocation, Form, useNavigate } from "@remix-run/react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
@@ -15,6 +13,12 @@ function MisCursos({ cursos }) {
 	const [activePath, setActivePath] = useState(location.pathname);
 	const [selectedCurso, setSelectedCurso] = useState(null);
 	const [selectedProyectos, setSelectedProyectos] = useState(null);
+
+	const transformedCourses = cursos.map((curso) => ({
+		...curso, // Include all original properties of the curso
+		temasTotales: curso.temas.length,
+		temasCompletados: curso.temas.filter((tema) => tema.completado === "true").length,
+	}));
 
 	useEffect(() => {
 		setActivePath(location.pathname);
@@ -50,7 +54,7 @@ function MisCursos({ cursos }) {
 						<p>No tienes cursos inscritos.</p>
 					</div>
 				) : (
-					cursos?.map((curso) => (
+					transformedCourses.map((curso) => (
 						<CollapsibleSection key={curso.idCurso} title={curso.nombreMateria}>
 							<div className="collapsible-content" style={{width:"100%"}}>
 								<div className="left-content">
@@ -88,36 +92,34 @@ function MisCursos({ cursos }) {
 									</Form>
 								</div>
 								<div className="right-content">
-								<div className="progress-box">
-									<CircularProgressbar
-										value='90'
-										maxValue='100'
-										styles={{
-											path: {
-												stroke: "var(--green-color)",
-											},
-										}}
-									/>
-									<div
-										className="Lato text-center fw-bold label-circle"
-										style={{ color: "var(--green-color)" }}
-									>
-										<span style={{ fontSize: "1em", marginRight: "0.1em" }}>
-											asdas
-										</span>
-										<span>de</span>
-										<span
-											style={{
-												fontSize: "1em",
-												marginLeft: "0.1em",
-												marginRight: "0.1em",
-											}}
+									<div className="progress-box">
+										<CircularProgressbar
+											value={curso.temasCompletados}
+											maxValue={curso.temasTotales}
+											styles={buildStyles({
+												pathColor: "var(--green-color)",
+											})}
+										/>
+										<div
+											className="Lato text-center fw-bold label-circle"
+											style={{ color: "var(--green-color)" }}
 										>
-											asdas
-										</span>
-										<span>temas completados</span>
+											<span style={{ fontSize: "2em", marginRight: "0.1em" }}>
+												{curso.temasCompletados}
+											</span>
+											<span>de</span>
+											<span
+												style={{
+													fontSize: "2em",
+													marginLeft: "0.1em",
+													marginRight: "0.1em",
+												}}
+											>
+												{curso.temasTotales}
+											</span>
+											<span>temas completados</span>
+										</div>
 									</div>
-								</div>
 								</div>
 							</div>
 						</CollapsibleSection>
